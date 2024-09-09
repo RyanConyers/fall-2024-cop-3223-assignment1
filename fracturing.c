82d302962790d273e8c1e6ec10e1f0edd9d65aa2
RyanConyers <ry367266@ucf.edu>
2024-09-09 04:07:04 +0000

    Switching it from "x1,y1,x2,y2" to "x1,x2,y1,y2"

--- fracturing.c
+++ fracturing.c
@@ -9,30 +9,33 @@
 // Defining PI to use in the coming equations
 #define PI 3.14159
 
-// Function to input the x and y coordinates for points 1 & 2
-void inputCoordinates(double* x, double* y, int pointNumber) {
-    printf("Enter x-coordinate #%d: ", pointNumber);
-    scanf("%lf", x);
-    printf("Enter y-coordinate #%d: ", pointNumber);
-    scanf("%lf", y);
+// Function to input the coordinates separately
+void inputCoordinates(double* x1, double* x2, double* y1, double* y2) {
+    printf("Enter x-coordinate #1: ");
+    scanf("%lf", x1);
+    printf("Enter x-coordinate #2: ");
+    scanf("%lf", x2);
+    printf("Enter y-coordinate #1: ");
+    scanf("%lf", y1);
+    printf("Enter y-coordinate #2: ");
+    scanf("%lf", y2);
 }
 
 // Function to print coordinates
-void printCoordinates(double x, double y, int pointNumber) {
-    printf("Point #%d entered: x = %.0f; y = %.0f\n", pointNumber, x, y);
+void printCoordinates(double x1, double x2, double y1, double y2) {
+    printf("Point #1 entered: x1 = %.0f; y1 = %.0f\n", x1, y1);
+    printf("Point #2 entered: x2 = %.0f; y2 = %.0f\n", x2, y2);
 }
 
 // Function to calculate the distance between two points
 double calculateDistance() {
-    double x1, y1, x2, y2;
+    double x1, x2, y1, y2;
 
-    inputCoordinates(&x1, &y1, 1);
-    inputCoordinates(&x2, &y2, 2);
+    inputCoordinates(&x1, &x2, &y1, &y2);
 
     double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 
-    printCoordinates(x1, y1, 1);
-    printCoordinates(x2, y2, 2);
+    printCoordinates(x1, x2, y1, y2);
 
     printf("The distance between the two points is %.5f\n", distance);
 
@@ -41,17 +44,15 @@ double calculateDistance() {
 
 // Function to calculate the perimeter of the circle
 double calculatePerimeter() {
-    double x1, y1, x2, y2;
+    double x1, x2, y1, y2;
 
-    inputCoordinates(&x1, &y1, 1);
-    inputCoordinates(&x2, &y2, 2);
+    inputCoordinates(&x1, &x2, &y1, &y2);
 
     double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     double radius = distance / 2.0;
     double perimeter = 2 * PI * radius;
 
-    printCoordinates(x1, y1, 1);
-    printCoordinates(x2, y2, 2);
+    printCoordinates(x1, x2, y1, y2);
 
     printf("The perimeter of the city encompassed by your request is %.5f\n", perimeter);
     return perimeter;
@@ -59,17 +60,15 @@ double calculatePerimeter() {
 
 // Function to calculate the area of the circle
 double calculateArea() {
-    double x1, y1, x2, y2;
+    double x1, x2, y1, y2;
 
-    inputCoordinates(&x1, &y1, 1);
-    inputCoordinates(&x2, &y2, 2);
+    inputCoordinates(&x1, &x2, &y1, &y2);
 
     double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     double radius = distance / 2.0;
     double area = PI * pow(radius, 2);
 
-    printCoordinates(x1, y1, 1);
-    printCoordinates(x2, y2, 2);
+    printCoordinates(x1, x2, y1, y2);
 
     printf("The area of the city encompassed by your request is %.5f\n", area);
     return area;
@@ -77,31 +76,27 @@ double calculateArea() {
 
 // Function to calculate the width (same as distance for a circle)
 double calculateWidth() {
-    double x1, y1, x2, y2;
+    double x1, x2, y1, y2;
 
-    inputCoordinates(&x1, &y1, 1);
-    inputCoordinates(&x2, &y2, 2);
+    inputCoordinates(&x1, &x2, &y1, &y2);
 
     double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 
-    printCoordinates(x1, y1, 1);
-    printCoordinates(x2, y2, 2);
+    printCoordinates(x1, x2, y1, y2);
 
     printf("The width of the city encompassed by your request is %.5f\n", distance);
     return distance;
 }
 
-// Function to calculate the height (same as distance and width of a circle)
+// Function to calculate the height (same as distance for a circle)
 double calculateHeight() {
-    double x1, y1, x2, y2;
+    double x1, x2, y1, y2;
 
-    inputCoordinates(&x1, &y1, 1);
-    inputCoordinates(&x2, &y2, 2);
+    inputCoordinates(&x1, &x2, &y1, &y2);
 
     double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 
-    printCoordinates(x1, y1, 1);
-    printCoordinates(x2, y2, 2);
+    printCoordinates(x1, x2, y1, y2);
 
     printf("The height of the city encompassed by your request is %.5f\n", distance);
     return distance;
